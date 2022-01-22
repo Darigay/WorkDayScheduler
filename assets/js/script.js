55d2c4dfc5d28ff0e365b78b76aa4776c1b5cc50
Divya <divya.arigay@gmail.com>
2022-01-22 12:01:45 -0700

    Timeblocks and Date Function

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const toDate = new Date();
+document.getElementById("currentDay").innerHTML= toDate;
+
+var currentHour = toDate.getHours();
+
+
+// save into local storage
+
+// function to update hours 
+
+// iterate over hourly timeblocks 
+
+// if-else statement - for pass present and future
+
+// set local storage for getItems for hour timeblocks
+
+$(".hour").each(function (indexInArray, valueOfElement) { 
+     
+});
+
