00deefb1a5b5b883b678c2bcf4ba53144e1c5d17
Divya <divya.arigay@gmail.com>
2022-01-22 14:16:25 -0700

    colorBlock function for past present future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const toDate = new Date();
-document.getElementById("currentDay").innerHTML= toDate;
+document.getElementById("currentDay").innerHTML = toDate;
 
-var currentHour = toDate.getHours();
+//var currentHour = toDate.getHours();
 
 
 // save into local storage
@@ -14,7 +14,65 @@ var currentHour = toDate.getHours();
 
 // set local storage for getItems for hour timeblocks
 
-$(".hour").each(function (indexInArray, valueOfElement) { 
-     
-});
+function colorBlocks() {
 
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function(){
+
+        var colorBlock = $(this).attr("id");
+
+        //colorBlock = parseInt(colorBlock[1]);
+        //console.log(colorBlock);
+
+
+       
+       if (colorBlock < currentHour) 
+        {  
+            
+           // $(this).css("background-color","red");
+ 
+           // $(".time-block").addclass("past");
+
+           $(this).addclass("past");
+            
+       }
+
+       else if (colorBlock === currentHour) 
+        { 
+           // $(this).css("background-color","red");
+           //$(".time-block").addclass("present");
+           $(this).addclass("present");
+         }
+//
+        else {
+
+           // $(".time-block").addclass("future");
+            //$(this).css("background-color","green");
+            $(this).addclass("future");
+
+        }
+
+    });
+}
+
+colorBlocks();
+
+// Save Buttons 
+
+$(".saveBtn").on("click",function(){
+
+    var task = $(this).parent().attr("id");
+    var taskInfo = $(this).siblings(".text").val();
+
+    localStorage.setItem (task, taskInfo);
+
+})
+
+
+// function 
+
+function getTaskInfo () {
+
+   
+}
